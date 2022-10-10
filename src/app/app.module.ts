a25b76e820f392ddefdbbe12b88f64ccd20ca23c
SANDHRAMARIYA <sandhramariya09@gmail.com>
2022-10-10 14:09:08 +0530

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostlistComponent } from './postlist/postlist.component';
 
+
+const appRoutes : Routes=[
+  {
+    path:"",component:PostlistComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +22,7 @@ import { PostlistComponent } from './postlist/postlist.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    ,RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
